cfe73cb38d63ac15f2fe31c2db15041849ca01d4
hoanglv <levanhoang0699@gmail.com>
2019-11-04 23:02:13 +0700

    update rigister

--- app/src/main/java/com/clearteam/phuotnhom/ui/login/LoginActivity.java
+++ app/src/main/java/com/clearteam/phuotnhom/ui/login/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.clearteam.phuotnhom.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.clearteam.phuotnhom.R;
+import com.clearteam.phuotnhom.ui.register.RegisterActivity;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView tvRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView() {
+        tvRegister = findViewById(R.id.tv_register);
+        tvRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_register:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
     }
 }
